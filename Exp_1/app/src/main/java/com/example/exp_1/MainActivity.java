63668a8e4f496c223d98f2d95dac57c871189016
Mohammed-Abed-Alkareem <1210708@student.birzeit.edu>
2024-07-17 13:01:21 +0300

    exp files

--- Exp_1/app/src/main/java/com/example/exp_1/MainActivity.java
+++ Exp_1/app/src/main/java/com/example/exp_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.exp_1;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            EditText name = findViewById(R.id.name);
+
+            String nameText = name.getText().toString();
+
+            TextView greeting = findViewById(R.id.greeting);
+            greeting.setText("Hello, " + nameText + "!");
+
         });
     }
 }
